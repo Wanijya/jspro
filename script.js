2ef369c7c68c08661d881b3365c1fa0bbc006253
Wanijya <wanijyabisen@gmail.com>
2023-09-27 17:36:05 +0530

    Js Practice 2

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-var rect = document.querySelector("#center");
-
-rect.addEventListener("mousemove", function(details){
-    var rectanglelocation = rect.getBoundingClientRect();
-    var insiderectval = details.clientX - rectanglelocation.left;
-
-    if(insiderectval<rectanglelocation.width/2){
-        var redcolor = gsap.utils.mapRange(0, rectanglelocation.width/2, 255, 0, insiderectval);
-        gsap.to(rect, {
-            backgroundColor: `rgb(${redcolor}, 0, 0)`,
-            ease: Power4,
-        }); 
-    }else{
-        var bluecolor = gsap.utils.mapRange(rectanglelocation.width/2, rectanglelocation.width, 0, 255, insiderectval);
-        gsap.to(rect, {
-            backgroundColor: `rgb(0, 0, ${bluecolor})`,
-            ease: Power4,
-        });
-    }
-})
-rect.addEventListener("mouseleave", function (){
-    gsap.to(rect, {
-        backgroundColor: "white"
-    })
-})
+window.addEventListener("mousemove", function (details) {
+  var rect = document.querySelector("#rect");
+  var xval = gsap.utils.mapRange(
+    0,
+    window.innerWidth,
+    100 + rect.getBoundingClientRect().width / 2,
+    window.innerWidth - (100 + rect.getBoundingClientRect().width / 2),
+    details.clientX
+  );
+  gsap.to("#rect", {
+    left: xval,
+    ease: Power3,
+  });
+});
