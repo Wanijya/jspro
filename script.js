150717beb49a42a213657bf6ac1a0f1ff920ce4f
Wanijya <wanijyabisen@gmail.com>
2023-11-22 13:35:44 +0530

    Bubble Game

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
-window.addEventListener("mousemove", function (details) {
-  var rect = document.querySelector("#rect");
-  var xval = gsap.utils.mapRange(
-    0,
-    window.innerWidth,
-    100 + rect.getBoundingClientRect().width / 2,
-    window.innerWidth - (100 + rect.getBoundingClientRect().width / 2),
-    details.clientX
-  );
-  gsap.to("#rect", {
-    left: xval,
-    ease: Power3,
-  });
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+
+function increaseScore() {
+  score += 10;
+  document.querySelector("#scoreval").textContent = score;
+}
+
+function getNewHit() {
+  hitrn = Math.floor(Math.random() * 10);
+  document.querySelector("#hitval").textContent = hitrn;
+}
+
+function makeBubble() {
+  var clutter = "";
+  for (var i = 1; i <= 200; i++) {
+    var rn = Math.floor(Math.random() * 10);
+    clutter += `<div class="bubble">${rn}</div>`;
+  }
+
+  document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+function runTimer() {
+  var timerint = setInterval(function () {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timerval").textContent = timer;
+    } else {
+      clearInterval(timerint);
+      document.querySelector("#pbtm").innerHTML = `<h1>Game Over</h1>`;
+    }
+  }, 1000);
+}
+
+document.querySelector("#pbtm").addEventListener("click", function (dets) {
+  var clickednum = Number(dets.target.textContent);
+  if(clickednum === hitrn){
+    increaseScore();
+    makeBubble();
+    getNewHit();
+  }
 });
+
+getNewHit();
+runTimer();
+makeBubble();
