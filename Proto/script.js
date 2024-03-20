3ea30958bb2dfc7639b9920677e1449349d0e9c6
Wanijya <wanijyabisen@gmail.com>
2024-03-20 20:30:36 +0530

    proto

---
+++ Proto/script.js
@@ -0,0 +1,20 @@
+// var obj = {
+//     fName: "John",
+// }
+// var obj2= {
+//     lName : "Smith",
+// }
+// obj2.__proto__ = obj;
+// console.log(obj2);
+
+// Proto ek object hai jo ki apne refrensa ko seach krta hai
+
+var obj = {
+    fName: "John",
+}
+obj2 = Object.create(obj);
+obj2.lName = "Smith";
+console.log(obj2)
+
+// __proto__ -> [[Prototype]]
+// prototype this is a different thing...
