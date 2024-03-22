51904ef31d2903103cd272d892fe2877f6fd6776
Wanijya <wanijyabisen@gmail.com>
2024-03-22 14:03:57 +0530

    Fetch

--- script.js
+++ script.js
@@ -1,36 +1,9 @@
-//HOF- Higher Order Functions
-//map
-function Map() {
-  let num = [10, 20, 30, 40, 50, 60];
-  let a = num.map((val) => {
-    return val + 10;
-  });
-  console.log(num);
-  console.log(a);
+// fetch
+function Fetch(){
+    fetch("https://jsonplaceholder.typicode.com/posts").then(response => {
+        return response.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log('error'));
 }
-Map();
-
-//filter
-function Filter() {
-  let num = [10, 20, 30, 40, 50, 60];
-  let a = num.filter((val) => {
-    if (val >= 10 && val <= 40) {
-      return val;
-    }
-  });
-  console.log(a);
-  console.log(num);
-}
-Filter();
-
-//reduce
-function Reduce(){
-    let num = [10,20,30,40,50,60]
-    let sum = num.reduce((acc,num)=>{
-        return acc + num;
-    }, 0);
-    console.log(sum);
-}
-Reduce();
-
-
+Fetch();
