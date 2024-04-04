51660080874c6a69a3964e884ed7c05247a3a380
Wanijya <wanijyabisen@gmail.com>
2024-04-05 01:56:14 +0530

    Fashion-website-ui

--- script.js
+++
@@ -1,9 +0,0 @@
-// fetch
-function Fetch(){
-    fetch("https://jsonplaceholder.typicode.com/posts").then(response => {
-        return response.json();
-    })
-    .then(data => console.log(data))
-    .catch(error => console.log('error'));
-}
-Fetch();
