5ece4910d935f2acb4c2a65b37535d2b474d527d
Wanijya <wanijyabisen@gmail.com>
2024-05-10 16:24:33 +0530

    Prototypal Inheritance

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
-// call, apply & bind
-// ye teen tareeka hai functions ko call karne ke, kisi object ko this maan kar.
+// Prototypal Inheritance :-
 
-const obj = {name: "Wani"};
-// call:-
-// function abcd(){
-//     console.log(this);
-// }
-// abcd.call(obj)
-
-// apply:- jb koii function parameter maang rha ho
-// function abcd(a,b,c){  
-//     console.log(this, a,b,c);
-// }
-// abcd.apply(obj, [1,2,3])
-
-// bind:- bind function ko chalata nhi hai balki ek new function deta hai return kr ke jis ko aap baad me chala skte ho.
-function abcd(){
-    console.log(this);
+function makeHuman(name, age){
+    this.name = name;
+    this.age = age;
 }
-const baadmechalanekeliyefunc = abcd.bind(obj);
-baadmechalanekeliyefunc();
+makeHuman.prototype.printMyName = function(){
+    console.log(this.name);
+}
+
+const human1 = new makeHuman("Wani", 21);
+const human2 = new makeHuman("Aaru", 19);
