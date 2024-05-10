00dbaf98e1852134feb65a22849a45c99cb1cd1a
Wanijya <wanijyabisen@gmail.com>
2024-05-10 15:40:38 +0530

    this val in diff - diff conditions

--- script.js
+++ script.js
@@ -1,18 +1,53 @@
-//How to loop an arrya
-howToLoop = function () {
-  var arr = [1, 2, 3, 4, 5];
-  arr.forEach(function (val) {
-    console.log(val);
-  });
-};
-// howToLoop();
+// This ki value alg alg condition me...
 
-//How to loop an arrya and do something with each element
+//global - Window
+console.log(this);
 
-howToLoopAndDoSomething = function () {
-  var arr = [1, 2, 3, 4, 5];
-  arr.forEach(function (val) {
-    console.log(val * 2);
-  });
-};
-// howToLoopAndDoSomething();
+// Function - Window
+function abcd(){
+    console.log(this);
+}
+abcd();
+
+// Method - object
+var obj = {
+    name: function(){ // this is a method.
+        console.log(this);
+    }
+}
+obj.name();
+
+// function inside method (es5) - windows
+var obj2 = {
+    sayName: function(){   // ye ek method hai.
+        console.log(this.age);
+        function childfnc(){
+            console.log(this);
+        }
+        childfnc();
+    },
+    age: 25,
+} 
+obj2.sayName();
+
+// function inside method (es6) - objects
+var obj3 = {
+    sayName: function() {   // ye ek method hai.
+        const chlid = () =>{
+            console.log(this)
+        }
+    },
+}
+obj3.sayName();
+
+//Constructor function - new blank object
+function add(){
+    console.log(this);
+} 
+const ans = new add();
+
+//event Listener - that element jispar event lis. laga ho
+document.querySelector("button")
+.addEventListener("click", function(){
+    console.log(this);
+})
