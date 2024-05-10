f14b8e0095fff1549e5dfed29d2a8a2168105131
Wanijya <wanijyabisen@gmail.com>
2024-05-10 15:53:54 +0530

    call, apply & bind

--- script.js
+++ script.js
@@ -1,53 +1,22 @@
-// This ki value alg alg condition me...
+// call, apply & bind
+// ye teen tareeka hai functions ko call karne ke, kisi object ko this maan kar.
 
-//global - Window
-console.log(this);
+const obj = {name: "Wani"};
+// call:-
+// function abcd(){
+//     console.log(this);
+// }
+// abcd.call(obj)
 
-// Function - Window
+// apply:- jb koii function parameter maang rha ho
+// function abcd(a,b,c){  
+//     console.log(this, a,b,c);
+// }
+// abcd.apply(obj, [1,2,3])
+
+// bind:- bind function ko chalata nhi hai balki ek new function deta hai return kr ke jis ko aap baad me chala skte ho.
 function abcd(){
     console.log(this);
 }
-abcd();
-
-// Method - object
-var obj = {
-    name: function(){ // this is a method.
-        console.log(this);
-    }
-}
-obj.name();
-
-// function inside method (es5) - windows
-var obj2 = {
-    sayName: function(){   // ye ek method hai.
-        console.log(this.age);
-        function childfnc(){
-            console.log(this);
-        }
-        childfnc();
-    },
-    age: 25,
-} 
-obj2.sayName();
-
-// function inside method (es6) - objects
-var obj3 = {
-    sayName: function() {   // ye ek method hai.
-        const chlid = () =>{
-            console.log(this)
-        }
-    },
-}
-obj3.sayName();
-
-//Constructor function - new blank object
-function add(){
-    console.log(this);
-} 
-const ans = new add();
-
-//event Listener - that element jispar event lis. laga ho
-document.querySelector("button")
-.addEventListener("click", function(){
-    console.log(this);
-})
+const baadmechalanekeliyefunc = abcd.bind(obj);
+baadmechalanekeliyefunc();
