543fe379af66451f263024c40b373a24f01d1f8c
Wanijya <wanijyabisen@gmail.com>
2025-02-27 03:20:03 +0530

    MAGASINN Website

--- script.js
+++ script.js
@@ -1,59 +1,93 @@
-// Merging two array of data fetched from separate APIs into a single list.
-const name = ["John", "Smith", "Doe", "Oregon", "Mary"]
-const lastname = ["Dock", "Ari", "Johnson", "Git", "Sow"]
-const fullname = [...name, ...lastname]
-console.log(fullname)
+const scroller = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+});
 
-// Filtering an array of objects to display items based on a search query (e.g. filtering products by name).
-const products = [
-    { name: "Apple", price: 1.5 },
-    { name: "Banana", price: 0.8 },
-    { name: "Orange", price: 1.5 },
-    { name: "Grapes", price: 1.0 },
-    { name: "Mango", price: 1.7 },
-]
-const newarr = products.filter(function(product){
-    return product.price === 1.5
-}) 
-console.log(newarr);
+var arrimg = [
+  "https://images.unsplash.com/photo-1638913662529-1d2f1eb5b526?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+  "https://images.unsplash.com/photo-1638913665258-ddd2bceafb30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+  "https://images.unsplash.com/photo-1638913660106-73b4bac0db09?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80",
+  "https://images.unsplash.com/photo-1638913662584-731da41f5a59?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+];
+var image1 = document.querySelector("#img1");
+var image2 = document.querySelector("#img2");
+var abc = 0;
+var dog = document.querySelector("#dog img");
+dog.addEventListener("mouseover", function () {
+  dog.setAttribute(
+    "src",
+    "https://magasinn.xyz/wordpress/wp-content/uploads/2021/12/img-ZASSHU02-480x480.png"
+  );
+});
+dog.addEventListener("mouseleave", function () {
+  dog.setAttribute(
+    "src",
+    "https://magasinn.xyz/wordpress/wp-content/uploads/2021/12/img-ZASSHU01-480x480.png"
+  );
+});
+image1.addEventListener("mouseover", function () {
+  image1.style.backgroundImage = `url(${arrimg[abc++]})`;
+  if (abc > 3) abc = 0;
+});
+var bc = 0;
+setInterval(function () {
+  image2.style.backgroundImage = `url(${arrimg[bc++]})`;
+  if (bc > 3) bc = 0;
+}, 1000);
+var icon = document.querySelector("#nav>h1");
+icon.addEventListener("click", function () {
+  document.location.reload(true);
+});
+var sitara = document.querySelector("#main2>h1");
+sitara.addEventListener("click", function () {
+  document.querySelector("#bg").scrollTo({
+    top: 600,
+    behavior: "smooth",
+  });
+});
+var rotate = 0;
+setInterval(function () {
+  sitara.style.transform = `rotate(${rotate++}deg)`;
+}, 30);
 
-// Mapping over an array of users data to create a list of user cards.
-const users = [
-    { name: "John Doe", age: 30 },
-    { name: "Jane Smith", age: 25 },
-    { name: "Alice Johnson", age: 28 },
-]
-let newArr = users.map(function(user) {
-    return `<div>Name: ${user.name}, Age: ${user.age}</div>`
-})
-console.log(newArr);
+var fullscreen = document.querySelector("#fullscreen");
+var bg = document.querySelector("#mains");
+var arrow = document.querySelector("#select");
+arrow.addEventListener("click", function () {
+  bg.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+  document.querySelector("#main2").style.opacity = `0`;
+  document.querySelector("#main3").style.opacity = `0`;
+  setTimeout(function () {
+    fullscreen.style.transform = `translateX(-198vh)`;
+    document.querySelector("#main").style.opacity = `0`;
+  }, 200);
+});
 
-// Grouping an array of objects by a specific property (e.g., grouping users by role.)
-const users2 = [
-    { name: "John Doe", role: "Developer" },
-    { name: "Jane Smith", role: "Designer" },
-    { name: "Alice Johnson", role: "Developer" },
-    { name: "Bob Brown", role: "Manager" },
-]
-let obj = {}
-users2.forEach(function(user) {
-    if(obj[user.role]){
-        obj[user.role].push(user);
-    }
-    else{
-        obj[user.role] = [];
-        obj[user.role].push(user);
-    }
-})
-console.log(obj);
+var change = document.querySelector("#fullscreen #flowimg");
+var changes = document.querySelector("#fullscreen #sideflow");
+var lab = 0;
+var lablab = 0;
+setInterval(() => {
+  if (lab > 1200) {
+    lab = 0;
+  }
 
-// Removing or updating a specific object in an array of data based on a unique ID.
-const users3 = [
-    { id: 1, name: "John Doe" },
-    { id: 2, name: "Jane Smith" },
-    { id: 3, name: "Alice Johnson" },
-]
-let newArr3 = users.filter(function(user) {
-    return user.id !== 2
-})
-console.log(newArr);
+  change.scrollTo({
+    left: lab,
+    behavior: "smooth",
+  });
+  lab++;
+}, 30);
+setInterval(() => {
+  if (lablab > 600) {
+    lablab = 0;
+  }
+  changes.scrollTo({
+    left: lablab,
+    behavior: "smooth",
+  });
+  lablab++;
+}, 30);
